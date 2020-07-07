08c1f9dfe180a99b4e82bcaa26e92c87fdf3d2dc
Kalix1984 <istvn.horvath@yahoo.com>
2020-07-07 11:35:46 +0200

    Certificate file kezelés és entity refaktorálás

--- Book4You/src/hu/ak_akademia/book4you/databases/CreateFilesForTesting.java
+++ Book4You/src/hu/ak_akademia/book4you/databases/CreateFilesForTesting.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 import hu.ak_akademia.book4you.entities.Address;
 import hu.ak_akademia.book4you.entities.PublicSpaceType;
+import hu.ak_akademia.book4you.entities.certificate.Certificate;
+import hu.ak_akademia.book4you.entities.certificate.Direction;
+import hu.ak_akademia.book4you.entities.certificate.Title;
 import hu.ak_akademia.book4you.entities.client.Client;
 import hu.ak_akademia.book4you.entities.client.EconomicClient;
 import hu.ak_akademia.book4you.entities.client.NaturalClient;
@@ -72,6 +75,37 @@ public class CreateFilesForTesting {
 		
 		for (Client client : clients) {
 			System.out.println(client.getName());
+		}
+		
+		// certificates.bin feltöltése teszt adatokkal
+		String url4 = "src/hu/ak_akademia/book4you/databases/certificates.bin";
+		List<Certificate> certificatesToSave = new ArrayList<>();
+		
+		Cashier actualCashier1 = new Cashier("Pénztáros Géza", "pg01", "E1234567");
+		Cashier actualCashier2 = new Cashier("Pénztáros Jolán", "pj01", "E1234567");
+		Client actualClient1 = new NaturalClient("Vásárló Tamás","vt01", address1);
+		Client actualClient2 = new NaturalClient("Vásárló Balázs","vb01", address2);
+		Client actualClient3 = new EconomicClient("Takarítószolgálat Kft.","tk01", address3);
+		
+		Certificate certificate1 = new Certificate(1, actualCashier1, Direction.INCOME, actualClient1, 4_000, Title.BUY_BOOK, "Robert C. Martin - Tiszta kód könyv");
+		Certificate certificate2 = new Certificate(2, actualCashier1, Direction.INCOME, actualClient2, 5_000, Title.BUY_BOOK, "Robert C. Martin - Túlélőkönyv programozóknak");
+		Certificate certificate3 = new Certificate(3, actualCashier2, Direction.OUTCOME, actualClient3, 3_000, Title.CLEANING, "Üzlet napi takarítása");
+		
+		certificatesToSave.add(certificate1);
+		certificatesToSave.add(certificate2);
+		certificatesToSave.add(certificate3);
+		
+		DataSaver certificateFileSaver = new FileHandler(url4);
+		certificateFileSaver.save(certificatesToSave);
+		
+		// certificates.bin-ből ArrayList-be töltés
+		DataLoader certificatesFileLoader = new FileHandler(url4);
+		
+		List<Certificate> certificates = (List<Certificate>) certificatesFileLoader.load();
+//		System.out.println(certificates.toString());
+		
+		for (Certificate certificate : certificates) {
+			System.out.println(certificate.getDate() + " " + certificate.getCashier().getName());
 		}
 	}
 }
--- Book4You/src/hu/ak_akademia/book4you/entities/certificate/Certificate.java
+++ Book4You/src/hu/ak_akademia/book4you/entities/certificate/Certificate.java
@@ -1,24 +1,28 @@
 package hu.ak_akademia.book4you.entities.certificate;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
 import hu.ak_akademia.book4you.entities.client.Client;
 import hu.ak_akademia.book4you.entities.user.Cashier;
 
-public class Certificate {
-	private int number;
-	private LocalDate date;
-	private Cashier cashier;
-	private Direction direction;
-	private Client client;
-	private int amount;
-	private Title title;
-	private String comment;
+public class Certificate implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private final int number;
+	private final LocalDate date;
+	private final Cashier cashier;
+	private final Direction direction;
+	private final Client client;
+	private final int amount;
+	private final Title title;
+	private final String comment;
 
 	public Certificate(int number, Cashier cashier, Direction direction, Client client, int amount, Title title,
 			String comment) {
 		this.number = number;
-		this.date = date.now();
+		this.date = LocalDate.now();
 		this.cashier = cashier;
 		this.direction = direction;
 		this.client = client;
@@ -58,4 +62,22 @@ public class Certificate {
 	public String getComment() {
 		return comment;
 	}
+
+	@Override
+	public String toString() {
+		StringBuilder text = new StringBuilder();
+		
+		text.append("number=" + number + "/n");
+		text.append("date=" + date + "/n");
+		text.append("cashierObj=" + cashier + "/n");
+		text.append("direction=" + direction + "/n");
+		text.append("clientObj=" + client + "/n");
+		text.append("amount=" + amount + "/n");
+		text.append("title=" + title + "/n");
+		text.append("comment=" + comment + "/n");
+		
+		return text.toString(); 
+	}
+	
+	
 }
