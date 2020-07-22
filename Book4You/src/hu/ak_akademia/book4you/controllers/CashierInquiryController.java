6400f1fe448502767e215bdb56365122fcd63a22
Kalix1984 <istvn.horvath@yahoo.com>
2020-07-22 17:41:46 +0200

    Lekérdezés refaktorálása ComboBox miatt

--- Book4You/src/hu/ak_akademia/book4you/controllers/CashierInquiryController.java
+++ Book4You/src/hu/ak_akademia/book4you/controllers/CashierInquiryController.java
@@ -1,11 +1,21 @@
 package hu.ak_akademia.book4you.controllers;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import hu.ak_akademia.book4you.entities.client.Client;
+import hu.ak_akademia.book4you.entities.client.Clients;
+import hu.ak_akademia.book4you.entities.client.ClientsHandler;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
-public class CashierInquiryController {
+public class CashierInquiryController implements Initializable{
 	@FXML
 	private Label openingBalance;
 	
@@ -35,5 +45,21 @@ public class CashierInquiryController {
 	
 	@FXML
 	private TableColumn tableViewAmount;
+	
+	@FXML
+	private ComboBox<Client> clientChooserComboBox;
 
+	private ClientsHandler clientsHandler;
+	private ObservableList<Client> clientOptions;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		setContentOfClientChooserComboBox();
+	}
+
+	private void setContentOfClientChooserComboBox() {
+		clientsHandler = new Clients("src/hu/ak_akademia/book4you/databases/clients.bin");
+		clientOptions = FXCollections.observableList(clientsHandler.load());
+		clientChooserComboBox.setItems(clientOptions);
+	}
 }
