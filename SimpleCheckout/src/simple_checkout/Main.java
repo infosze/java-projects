4bd7d998e2260603b3de061344e4cd4a793b5f80
Rózsa Tamás <tusisuti@gmail.com>
2021-02-19 17:48:53 +0100

    Initial commit -first

---
+++ SimpleCheckout/src/simple_checkout/Main.java
@@ -0,0 +1,9 @@
+package simple_checkout;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+
+}
---
+++ SimpleCheckout/src/simple_checkout/menu_items/Menu.java
@@ -0,0 +1,5 @@
+package simple_checkout.menu_items;
+
+public class Menu {
+
+}
